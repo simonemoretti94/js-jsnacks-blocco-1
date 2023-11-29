63c7936c8291808ecba22654df9320f6bc24ff03
simonemoretti94 <simone.moretti.944@gmail.com>
2023-11-29 12:35:04 +0100

    5th concluded

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -1,1 +1,36 @@
 //script
+
+const vettore = [];
+
+for (let i = 0; i < 10; i++) {
+    let n = prompt('Write below a number');
+
+    if (n === NaN) {
+        console.log('not a number');
+        // continue;
+    }
+    else if (n % 2 === 0) {
+        console.log(n, ' is even');
+    }
+    else {
+        console.log(n, ' is odd!');
+        vettore.push(n);
+    }
+}
+
+i = 0;
+
+if (vettore.length != 0) {
+    
+    for(i = 0; i < vettore.length; i++) {
+        console.log('vettore element pos: ', i, ' is ',vettore[i]);
+    }
+
+    alert('Please open inspector\'s console to check odd numbers');
+
+}
+else {
+    alert('You wrote only even  numbers!');
+}
+
+alert('Shalom!');
