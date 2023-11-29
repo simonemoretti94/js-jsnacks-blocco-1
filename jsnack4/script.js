51390ed04b3aff272379c6f1c5ffb4b86af75dfb
simonemoretti94 <simone.moretti.944@gmail.com>
2023-11-29 12:20:45 +0100

    2nd commit on snack #4

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -6,7 +6,28 @@ const guest_name = prompt('Please write below you name; \nMake sure to capitaliz
 
 console.log('Guest name: ', guest_name, ' Guest list length: ', guests.length);
 
+//declaring counter for utility purpose
+let c = -1;
+
+//iterating on the array
 for (let i = 0; i < guests.length; i++) {
+
+    console.log('Actual guest: ',guests[i]);
     
-   
+    //if in order to verify input ad array index
+    if (guest_name === guests[i]) {
+
+        c = i;
+    }
 }
+
+
+console.log('c: ',c);
+
+if (c > -1) {
+    alert(`${guest_name} found at position #${c}, you can have fun with us!.`);
+    
+}
+else {
+    alert(`${guest_name} not found, you cannont enter the party.`)  
+ }
